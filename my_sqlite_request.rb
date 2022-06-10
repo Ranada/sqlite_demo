a8962cf1c2bea9019377c746fe787227521f4027
Neil Ranada <neil.ranada@gmail.com>
2022-06-10 13:36:16 -0700

    Refactor Order methods

--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -49,21 +49,50 @@ end
 class OrderProcess
     def run(request)
         if request.order == nil
-            request.order = ["First column (default)", "ASC"]
-            request.query_result.sort_by! { |hash| hash.values.first }
+            default_order(request)
         else
-            column_name = request.order[0]
-            order = request.order[1]
-            if order == "ASC"
-                partitioned_array = request.query_result.partition { |key, value| key[column_name] != nil }
-                partitioned_array[0].sort_by! { |key, value|  key[column_name]}
-                request.query_result = partitioned_array.flatten
-            elsif order == "DESC"
-                partitioned_array = request.query_result.partition { |key, value| key[column_name] != nil }
-                partitioned_array[0].sort_by! { |key, value|  key[column_name]}.reverse
-                request.query_result = partitioned_array.flatten
-            end
+            cli_entry_order(request)
         end
+    end
+
+    def default_order(request)
+        request.order = ["First column (default)", "ASC"]
+        request.query_result.sort_by! { |hash| hash.values.first }
+    end
+
+    def cli_entry_order(request)
+        column_name = request.order[0]
+        order = request.order[1]
+        order_asc(request, column_name) if order == "ASC"
+        order_desc(request, column_name) if order == "DESC"
+    end
+
+    def order_asc(request, column_name)
+        if request.query_result.each.any? { |key, value| key[column_name] == nil }
+            partition_nil_asc(request, column_name)
+        else
+            request.query_result = request.query_result.sort_by! { |key, value|  key[column_name]}
+        end
+    end
+
+    def partition_nil_asc(request,column_name)
+        partitioned_array = request.query_result.partition { |key, value| key[column_name] != nil }
+        partitioned_array[0] = partitioned_array[0].sort_by! { |key, value|  key[column_name]}
+        request.query_result = partitioned_array.flatten
+    end
+
+    def order_desc(request, column_name)
+        if request.query_result.each.any? { |key, value| key[column_name] == nil }
+            partition_nil_desc(request, column_name)
+        else
+            request.query_result = request.query_result.sort_by! { |key, value|  key[column_name]}.reverse
+        end
+    end
+
+    def partition_nil_desc(request, column_name)
+        partitioned_array = request.query_result.partition { |key, value| key[column_name] != nil }
+        partitioned_array[0] = partitioned_array[0].sort_by! { |key, value|  key[column_name]}.reverse
+        request.query_result =partitioned_array.flatten
     end
 end
 
